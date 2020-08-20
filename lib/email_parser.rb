f5aa4435c600b71d1ae7ecbabdb5e204b0acb814
Marques Avery <averymarques4@gmail.com>
2020-08-20 18:47:26 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -8,8 +8,11 @@ class EmailAddressParser
   attr_accessor :email_addresses
 
   def initialize(emails)
-    @email_addresses = emails.split(",")
+    @email_addresses = emails
+  end
 
+  def parse
+    @email_addresses.split(",")
   end
 
 
